426fe189f3b9fdf419e6c850f8dfcb38001a55f1
IvanAnka <ivanakna05@gmail.com>
2023-06-18 03:09:17 -0400

    primer

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/open_card', methods=['POST'])
+def open_card():
+    # Aquí puedes agregar la lógica para la animación de apertura de la carta y mostrar la imagen de la carta.
+    # Puedes usar una biblioteca como CSS o JavaScript para la animación.
+
+    # Por simplicidad, aquí solo se devuelve un mensaje de éxito.
+    return '¡La carta se ha abierto!'
+
+if __name__ == '__main__':
+    app.run()
